82e60515c61367cad60acb19029a73ff6dc3f804
Priyank Gupta <priyank.gupta@vinsol.com>
2011-11-18 13:02:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PriyankDepot::Application.config.secret_token = '794d5d3c402e751c6d503e3120d6839eb5957f20363b401d46f7ceb0d1ff32abb036cf360c3e9c38bc7292f33f6b53bdec8609bc6ff769ebefac77cad6f49b97'
