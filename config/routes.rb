207a61f77051f755b021e56ef57f9655f08b6d42
Priyank Gupta <priyank.gupta@vinsol.com>
2011-11-19 19:05:48 +0530

    till task G

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 PriyankDepot::Application.routes.draw do
+  resources :orders
+
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
+  
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
